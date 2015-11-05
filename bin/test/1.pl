9f01cde66ad346b24ffc0cc88e0570193cdd4a5d
Andrew Gideon <andrewPTA@gideon.org>
2015-11-05 00:16:45 -0500

    This is an initial experiment with reading .xlsx files using Spreadsheet::Read.  This reads the data, performs a sample transformation (with room for more to be added), and then writes it out.
    More transformations will likely be needed.
    
    Writing write now is just for debugging to stdout.  This will
    ultimately have to write a CSV or spreadsheet format.

---
+++ bin/test/1.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl -w
+
+BEGIN
+{
+#	if ($ENV{'PERL5LIB'})
+#	{
+#		unshift(@INC, split(/:/, $ENV{'PERL5LIB'}));
+#	}
+}
+
+use strict;
+use Data::Dumper;
+use Spreadsheet::Read;
+
+
+
+sub extractTeacherGrade($)
+{
+#	print "extractTeacherGrade()\n";
+	my @row = @_;
+	my $grade;
+	if ($row[3] =~ /(\d+)/)
+	{
+		$grade = $1;
+	}
+	push(@row, $grade);
+	return(@row);
+}
+
+
+sub processRow($@)
+{
+	my ($transforms, @row) = @_;
+	if (defined($transforms))
+	{
+		foreach my $transform (@$transforms)
+		{
+			@row = $transform->(@row);
+		}
+	}
+	return(@row);
+}
+
+sub main()
+{
+	# Collect the set of transformations to perform on each row
+	my $transforms = [];
+	push(@$transforms, \&extractTeacherGrade);
+
+	my $staff = ReadData('data/PTAStaff.2015-10-21.xlsx') or die "Cannot read file: $!";
+	my $page = $staff->[1];
+	my $pageMaxRow = $page->{maxrow};
+	my $pageMaxCol = $page->{maxcol};
+
+	# Loop over rows in sheet
+	for my $row (2..$pageMaxRow) # Note: Row 1 is column headers
+	{
+		my @rowData = Spreadsheet::Read::cellrow($page, $row);
+		@rowData = processRow($transforms, @rowData);
+		print join(', ', map { defined($_) ? $_ : '***'; } @rowData), "\n";
+	}
+}
+
+main();
+
+
