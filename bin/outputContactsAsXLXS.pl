717ea5befc371486dd73914a0e76318beeea33d2
Andrew Gideon <andrewPTA@gideon.org>
2020-10-16 21:59:50 -0400

    Remove old test code.

--- bin/outputContactsAsXLXS.pl
+++ bin/outputContactsAsXLXS.pl
@@ -7,37 +7,6 @@ use Getopt::Long;
 use DBI qw(:sql_types);
 
 
-sub main1()
-{
-    my $csv = Text::CSV->new ({ binary => 1, auto_diag => 1 });
-    my $out = *STDOUT;
-    $csv->say($out, [1, 2, 'Hi, there', 3]);
-}
-
-sub main2()
-{
-    
-
-    my ($outputFilename);
-    GetOptions('out=s'			=>	\$outputFilename,
-	);
-
-    my $sheetout;
-    if (defined($outputFilename))
-    {
-	$sheetout = Excel::Writer::XLSX->new($outputFilename) or
-	    die "Unable to open XLSX file $outputFilename for output: $!";
-    }
-    else
-    {
-	binmode(STDOUT);
-	$sheetout = Excel::Writer::XLSX->new( \*STDOUT );
-    }
-
-    my $pageout = $sheetout->add_worksheet('First Tab') or die "Unable to create sheet: $!";
-    $pageout->write_row(0, 0, [1, 2, 'Hi, there', 3]);
-}
-
 sub getContacts
 {
     my ($dbName, $dbHostname, $dbPort, $dbUsername, $dbPassword, 
