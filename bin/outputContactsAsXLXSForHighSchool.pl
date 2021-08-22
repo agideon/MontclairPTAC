0b0760a214f64d64261c5572f142a63806ebd5c8
Andrew Gideon <andrewPTA@gideon.org>
2021-08-22 16:08:22 -0400

    This is a variation of outputContactsAsXLXS.pl which outputs CSV and also outputs grades 8-11 for all schools.  It is intended as a work-around to let us reach HS students before the district has provided us with this year's data.  It therefore uses last year's data, but assumes that everyone has been promoted and that ex-8th-graders have all moved into the high school.
    It also outputs CSV instead of XLXS, which means that the name
    should probably be changed.  This is because we're no longer
    populating the old web site but instead something more modern
    (ie. mailchimp or memberhub).
    
    The XLXS code remains at least for now, though, in case we need that
    other format.

---
+++ bin/outputContactsAsXLXSForHighSchool.pl
@@ -0,0 +1,279 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Text::CSV;
+use Excel::Writer::XLSX;
+use Getopt::Long;
+use DBI qw(:sql_types);
+use FileHandle;
+
+sub getContacts
+{
+    my ($dbName, $dbHostname, $dbPort, $dbUsername, $dbPassword, 
+	$useForDirectory, $useForEmailBcast, 
+	$writeHeaderRow, $writeDataRow, $finish) = @_;
+    my @rval;
+    my $dsn = "DBI:mysql:database=$dbName;host=$dbHostname;port=$dbPort;mysql_multi_statements=1";
+    my $dbh = DBI->connect($dsn, $dbUsername, $dbPassword,
+			   {
+			       AutoCommit => 0,
+			   },
+	) or die("Unable to connect to db: " . $DBI::errstr);
+    
+    my $query = <<FINI;
+
+    select
+		distinct
+
+		if(p.number is not null, p.number, "-") as "Phone Num",
+		if(scp.cellular>0 and scp.cellular is not null, "Cell", "-") as "Cell?",
+		if(scp.home>0 and scp.cellular is not null, "Home", "-") as "Home?",
+		if(scp.prime>0 and scp.cellular is not null, "Main-number", "-") as "Main Number?",
+
+		if(e.address is not null,e.address,"-") as "Email",
+
+		sc.first_name as "Guardian first name",
+		sc.last_name as "Guardian last name",s.first_name "Student first name",
+		s.last_name "Student last name",s.grade,
+
+		if(sch.canonical_school_name is not null, sch.canonical_school_name, "-") as "School",
+
+		if(h.room is not null, h.room, "-") as "Room",
+		if(h.teacher is not null, h.teacher, "-") as "Teacher"
+
+
+	from 
+		student_contact sc join student_student_contact ssc on sc.student_contact_id = ssc.student_contact_id
+		join student s on ssc.student_id = s.student_id
+
+		left outer join student_contact_phone scp on sc.student_contact_id = scp.student_contact_id
+		left outer join phone p on scp.phone_id = p.phone_id
+
+		left outer join student_contact_email sce on sc.student_contact_id = sce.student_contact_id
+		left outer join email e on sce.email_id = e.email_id
+
+		left outer join homeroom h on s.homeroom_id = h.homeroom_id
+
+		left outer join school sch on s.school_id = sch.school_id
+
+
+	where
+		s.grade in (8,9,10,11)
+
+		/* Avoid contact-free rows - only accept rows with either a phone or email */
+		and ((p.number is not null) or (e.address is not null))
+
+
+
+    /* Directory use */
+		and ((? = 1) OR (sc.use_in_directory = 1))
+
+    /* Email use */
+
+		and ((? = 1) OR
+			(
+			  (sc.use_in_broadcast = 1) 
+			and (e.address is not null)
+			and (trim(e.address) != "")
+			))
+	order by sc.last_name, sc.first_name
+
+
+FINI
+
+    my $statement = $dbh->prepare($query) or die("Unable to prepare query " . $query . ": " . $dbh->err . ": " . $dbh->errstr);
+    {
+	my $pindex = 0;
+	$statement->bind_param(++$pindex, !$useForDirectory); # Not 1 if listing those in directory
+	$statement->bind_param(++$pindex, !$useForEmailBcast); # Not 1 if listing those in email
+    }
+    {
+	my $query = $statement->{Statement}; # Just used for error reporting
+	$statement->execute() or die("Unable to execute query " . $query . ": " . $statement->err . ": " . $statement->errstr);
+	
+	my $resultSet = 0;
+	do
+	{
+	    $resultSet++;
+	    my $rowCount = $statement->rows;
+	    my $columnCount = $statement->{'NUM_OF_FIELDS'} or 0;
+	    
+	    # It seems that an insert returns a row count of 1 (or perhaps more than that for multivalued inserts)
+	    # but a column count of 0 or undef.  So both should be checked (though perhaps only columnCount
+	    # is strictly required?).
+	    if (($rowCount > 0) && ($columnCount))
+	    {
+		# Column Headers
+		my $columnNames = $statement->{NAME};
+		$writeHeaderRow->($columnNames);
+
+		# Data Rows
+		while (my @row = $statement->fetchrow_array())
+		{
+		    $writeDataRow->(\@row);
+		}
+	    }
+	    else
+	    {
+		print STDERR "Skipping retrieval for result set ", $resultSet, ' with row count:', $rowCount, ' and column count: ', $columnCount, "\n";
+	    }
+	} while ($statement->more_results);
+    }
+
+
+    $dbh->commit;
+    $finish->();
+}
+
+sub enquoteOptionally($)
+{
+    my ($text) = @_;
+    if ($text =~ /[\",]/)
+    {
+	$text = enquote($text);
+    }
+    return($text);
+}
+
+sub enquote($)
+{
+    my ($text) = @_;
+    $text =~ s/\"/\\\\"/g;
+    return('"' . $text . '"');
+}
+
+
+sub setupCSVOutput
+{
+    my ($outputFilename) = @_;
+
+    my $out = FileHandle->new($outputFilename, O_WRONLY|O_TRUNC) or
+	die "Unable to open CSV file $outputFilename for output: $!";
+
+    my $writeHeaderRow = sub
+    {
+	my ($columnNames) = @_;
+	print $out join(', ', map { enquoteOptionally($_); } @$columnNames), "\n";
+    };
+
+    my $writeDataRow = sub
+    {
+	my ($row) = @_;
+	print $out join(', ', map { enquoteOptionally($_); } @$row), "\n";
+    };
+
+    my $finish = sub 
+    {
+	$out->close();
+    };
+
+    return($writeHeaderRow, $writeDataRow, $finish);
+}
+
+
+sub setUpXLSXOutput
+{
+    my ($outputFilename) = @_;
+
+    my $sheetout;
+    if (defined($outputFilename))
+    {
+	$sheetout = Excel::Writer::XLSX->new($outputFilename) or
+	    die "Unable to open XLSX file $outputFilename for output: $!";
+    }
+    else
+    {
+	binmode(STDOUT);
+	$sheetout = Excel::Writer::XLSX->new( \*STDOUT );
+    }
+
+    my $pageout = $sheetout->add_worksheet('First Tab') or die "Unable to create sheet: $!";
+
+    my $headerFormat = $sheetout->add_format('align' => 'center', 'bold' => 1);
+    my $dataFormat = $sheetout->add_format('align' => 'left', 'bold' => 0);
+
+    my $sheetRowIndex = 0;
+    my $writeHeaderRow = sub
+    {
+	my ($columnNames) = @_;
+	$pageout->write_row($sheetRowIndex++, 0, $columnNames, $headerFormat);
+    };
+    my $writeDataRow = sub
+    {
+	my ($row) = @_;
+	$pageout->write_row($sheetRowIndex++, 0, 
+			    $row, $dataFormat);
+    };
+
+    my $finish = sub
+    {
+	$pageout->set_column(0, 0, 15);
+	$pageout->set_column(1, 2, 6);
+	$pageout->set_column(3, 3, 13);
+	$pageout->set_column(4, 8, 25);
+	$pageout->set_column(9, 9, 5);
+	$pageout->set_column(10, 10, 10);
+	$pageout->set_column(11, 11, 5);
+	$pageout->set_column(12, 12, 18);
+	$pageout->freeze_panes(1, 0); # Freeze (or float) first row
+	$sheetout;
+    };
+
+    return($writeHeaderRow, $writeDataRow, $finish);
+
+
+}
+
+
+sub main()
+{
+    my ($dbUsername, $dbPassword, $dbName, $dbHostname, $dbPort, $useForEmailBcast, $useForDirectory);
+    my ($outputFilename);
+    my @inputErrors;
+    GetOptions('out=s'			=>	\$outputFilename,
+	       'username|u=s'	=>	\$dbUsername,
+	       'password|p=s'	=>	\$dbPassword,
+	       'db|dbname=s'	=>	\$dbName,
+	       'dhhost|host=s'	=>	\$dbHostname,
+	       'dbport|port=s'	=>	\$dbPort,
+	       'email'		=>	\$useForEmailBcast,
+	       'directory'	=>	\$useForDirectory,
+	);
+
+	if (!$dbUsername) { push(@inputErrors, "DB Username Required"); }
+	if (!$dbPassword) { push(@inputErrors, "DB Password Required"); }
+	if (!$dbName) { push(@inputErrors, "DB Name Required"); }
+	if (!$dbHostname) { $dbHostname = '127.0.0.1'; }
+	if (!$dbPort) { $dbPort = 3306; }
+    	if (defined($useForEmailBcast) && defined($useForDirectory))
+	{
+	    push(@inputErrors, "Only one of --email and --directory may be specified")
+	}
+	elsif (!defined($useForEmailBcast) && !defined($useForDirectory))
+	{
+	    push(@inputErrors, "One of --email and --directory must be specified")
+	}
+
+
+	if (scalar(@inputErrors) > 0)
+	{
+		print STDERR <<FINI;
+Usage: $0 --out <output xlsx file> --username <dbusername> --password <dbpassword> --dbname <dbname> --dbhost <db hostname> --dbport <db port #> [--directory] [--email]
+FINI
+		print "\t", join("\n\t", @inputErrors), "\n";
+		die("\tCommand line options incorrect\n");
+	}
+
+
+
+
+#    my ($writeHeaderRow, $writeDataRow, $finish) = setUpXLSXOutput($outputFilename);
+    my ($writeHeaderRow, $writeDataRow, $finish) = setupCSVOutput($outputFilename);
+    getContacts($dbName, $dbHostname, $dbPort, $dbUsername, $dbPassword, 
+		$useForDirectory, $useForEmailBcast, 
+		$writeHeaderRow, $writeDataRow, $finish);
+
+
+}
+
+main();
